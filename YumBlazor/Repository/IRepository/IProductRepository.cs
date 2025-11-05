b4e4b7d877cbb21f3331d4374b7e4c7158fa1104
Michał Przywojski <michal.przywojski@intelo.pl>
2025-11-05 17:06:04 +0100

    Add Product

---
+++ YumBlazor/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository;
+
+public interface IProductRepository
+{
+    public Product Create(Product obj);
+    public Product Update(Product obj);
+    public bool Delete(int id);
+    public Product Get(int id);
+    public IEnumerable<Product> GetAll();
+
+    public Task<Product> CreateAsync(Product obj);
+    public Task<Product> UpdateAsync(Product obj);
+    public Task<bool> DeleteAsync(int id);
+    public Task<Product> GetAsync(int id);
+    public Task<IEnumerable<Product>> GetAllAsync();
+}
---
+++ YumBlazor/Repository/ProductRepository.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YumBlazor.Data;
+using YumBlazor.Repository.IRepository;
+
+namespace YumBlazor.Repository;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public ProductRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public Product Create(Product obj)
+    {
+        _db.Products.Add(obj);
+        _db.SaveChanges();
+        return obj;
+    }
+
+    public bool Delete(int id)
+    {
+        var obj = _db.Products.FirstOrDefault(x => x.Id == id);
+        if (obj != null)
+        {
+            _db.Products.Remove(obj);
+             return _db.SaveChanges() > 0;
+        }
+        return false;
+    }
+
+    public Product Get(int id)
+    {
+        var obj = _db.Products.FirstOrDefault(x => x.Id == id);
+        if (obj == null)
+        {
+            return new Product();
+        }
+        return obj;
+    }
+
+    public IEnumerable<Product> GetAll()
+    {
+        return [.. _db.Products];
+    }
+
+    public Product Update(Product obj)
+    {
+        var objFromDb = _db.Products.FirstOrDefault(x => x.Id == obj.Id);
+        if (objFromDb is not null)
+        {
+            objFromDb.Name = obj.Name;
+            objFromDb.Description = obj.Description;
+            objFromDb.ImageUrl = obj.ImageUrl;
+            objFromDb.CategoryId = obj.CategoryId;
+            objFromDb.Price = obj.Price;
+            _db.Products.Update(objFromDb);
+            _db.SaveChanges();
+            return objFromDb;
+        }
+        return obj;
+    }
+
+    public async Task<Product> CreateAsync(Product obj)
+    {
+        await _db.Products.AddAsync(obj);
+        await _db.SaveChangesAsync();
+        return obj;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var obj = await _db.Products.FirstOrDefaultAsync(x => x.Id == id);
+        if (obj != null)
+        {
+            _db.Products.Remove(obj);
+             return (await _db.SaveChangesAsync()) > 0;
+        }
+        return false;
+    }
+
+    public async Task<Product> GetAsync(int id)
+    {
+        var obj = await _db.Products.FirstOrDefaultAsync(x => x.Id == id);
+        if (obj == null)
+        {
+            return new Product();
+        }
+        return obj;
+    }
+
+    public async Task<IEnumerable<Product>> GetAllAsync()
+    {
+        return await _db.Products.ToListAsync();
+    }
+
+    public async Task<Product> UpdateAsync(Product obj)
+    {
+        var objFromDb = await _db.Products.FirstOrDefaultAsync(x => x.Id == obj.Id);
+        if (objFromDb is not null)
+        {
+            objFromDb.Name = obj.Name;
+            objFromDb.Description = obj.Description;
+            objFromDb.ImageUrl = obj.ImageUrl;
+            objFromDb.CategoryId = obj.CategoryId;
+            objFromDb.Price = obj.Price;
+            _db.Products.Update(objFromDb);
+            await _db.SaveChangesAsync();
+            return objFromDb;
+        }
+        return obj;
+    }
+}
